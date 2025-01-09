ab722f918ef2c3f598b32f66d8086082e862edae
フォンテイン <119344403+Fontainwr@users.noreply.github.com>
2025-01-09 21:18:31 +0900

    done
    done

---
+++ RPS/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const choices = ["rock", "paper", "scissors"];
+    const choiceButton = document.querySelectorAll(".choice");
+    const playerChoiceSpan = document.getElementById("player-choice");
+    const computerChoiceSpan = document.getElementById("computer-choice");
+    const resultSpan = document.getElementById("result");
+    const restartButton = document.getElementById("restart");
+
+
+    
+})
